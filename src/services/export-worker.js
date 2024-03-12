2ab87ea36ba335dcfdeb4390c1b855e3ca4ac4ed
Diego Goulart <diegogoulart.aws@gmail.com>
2024-03-12 17:57:04 -0300

    parse google key

--- src/services/export-worker.js
+++ src/services/export-worker.js
@@ -6,7 +6,7 @@ import { prisma } from '@/lib/prisma'
 
 const serviceAccountAuth = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY, // Assegura que quebras de linha são tratadas corretamente
+  key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Assegura que quebras de linha são tratadas corretamente
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 })
 
